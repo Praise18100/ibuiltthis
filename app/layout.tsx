4ba7ce54e2c6399a19f6a441e78fbaa9a70e5101
Praise18100 <akenroyepraise@gmail.com>
2026-04-12 17:04:46 +0100

    feat: initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+
+export const metadata: Metadata = {
+  title: "iBuiltThis",
+  description: "iBuiltThis is a platform for building and sharing your own projects with the world",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      className={`${inter.className} h-full antialiased`}
+    >
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  );
+}
